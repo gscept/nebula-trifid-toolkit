a948c1cb1ed0d3bdde7fb13d98eb56b935ae33ad
johannes <johannes@gscept.com>
2016-11-04 20:07:19 +0100

    - moved everything toolkit out of the main repo

---
+++ code/qttoolkit/contentbrowser/code/widgets/meshes/meshhandler.cc
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+//  meshitemhandler.cc
+//  (C) 2012-2016 Individual contributors, see AUTHORS file
+//------------------------------------------------------------------------------
+#include "stdneb.h"
+#include "meshhandler.h"
+
+namespace Widgets
+{
+__ImplementClass(Widgets::MeshHandler, 'MEIH', Widgets::BaseHandler);
+
+//------------------------------------------------------------------------------
+/**
+*/
+MeshHandler::MeshHandler()
+{
+	// empty
+}
+
+//------------------------------------------------------------------------------
+/**
+*/
+MeshHandler::~MeshHandler()
+{
+	// empty
+}
+
+} // namespace Widgets
---
+++ code/qttoolkit/leveleditor2/code/main.cc
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+#include "stdneb.h"
+#include "leveleditor2app.h"
+#include "libqimg/qdevilplugin.h"
+Q_IMPORT_PLUGIN(qdevil);
+
+//------------------------------------------------------------------------------
+/**
+*/
+
+int __cdecl
+main(int argc, const char** argv)
+{
+	Util::CommandLineArgs args(argc, argv);
+	LevelEditor2::LevelEditor2App app;
+	app.SetCompanyName("gscept");
+	app.SetAppTitle("NebulaT Level Editor");	
+	app.SetCmdLineArgs(args);
+	if (app.Open())
+	{
+		app.Run();
+		app.Close();
+	}
+	app.Exit();
+}
